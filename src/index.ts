bebed08f7a8390a59072412117ed673b117739df
klegul <gregor@klegul.dev>
2025-10-02 18:59:31 +0200

    Add .json endpoint that returns url in json format

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,41 @@ export default {
 			width = parseInt(url.searchParams.get('width')!!);
 		}
 
-		switch (url.pathname) {
+		let bucket: R2Bucket;
+		let bucketPublicUrl: string;
+
+		switch (url.pathname.split('.json')[0]) {
 			case '/animal-avatars':
-				return getRandomImage(env.ANIMAL_AVATARS, env.ANIMAL_AVATARS_PUBLIC_URL, width);
+				bucket = env.ANIMAL_AVATARS;
+				bucketPublicUrl = env.ANIMAL_AVATARS_PUBLIC_URL;
+				break;
 			default:
 				return new Response('Not found', { status: 404 });
 		}
+
+		const imageUrl = await getRandomImage(bucket, bucketPublicUrl, width);
+		if (!imageUrl) {
+			return new Response('No images found', { status: 404 });
+		}
+
+		if (url.pathname.endsWith('.json')) {
+			return new Response(
+				JSON.stringify({
+					url: imageUrl,
+				}),
+				{ headers: { 'Content-Type': 'application/json' } },
+			);
+		}
+
+		return Response.redirect(imageUrl);
 	},
 } satisfies ExportedHandler<Env>;
 
-async function getRandomImage(bucket: R2Bucket, bucketPublicUrl: string, width?: number): Promise<Response> {
+async function getRandomImage(bucket: R2Bucket, bucketPublicUrl: string, width?: number): Promise<string | undefined> {
 	const listResponse = await bucket.list({ limit: 100 });
 
 	if (!listResponse.objects || listResponse.objects.length === 0) {
-		return new Response('No images found', { status: 404 });
+		return undefined;
 	}
 
 	const randomIndex = Math.floor(Math.random() * listResponse.objects.length);
@@ -31,7 +52,5 @@ async function getRandomImage(bucket: R2Bucket, bucketPublicUrl: string, width?:
 		imageTransformString = `/cdn-cgi/image/width=${width}`;
 	}
 
-	const imageUrl = `${bucketPublicUrl}${imageTransformString}/${randomObject.key}`;
-
-	return Response.redirect(imageUrl);
+	return `${bucketPublicUrl}${imageTransformString}/${randomObject.key}`;
 }
